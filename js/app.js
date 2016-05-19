de441b3bb858e84c18a0f109ad9196c3ce098d8d
meshnesh <tonny.nesh@gmail.com>
2016-05-19 03:29:00 +0300

    added navigation bar and sections

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+$(document).ready(function () {
+    $(window).bind('scroll', function () {
+        var navHeight = $(window).height() - 70;
+        if ($(window).scrollTop() > navHeight) {
+            $('nav').addClass('fixed');
+        } else {
+            $('nav').removeClass('fixed');
+        }
+    });
+});
